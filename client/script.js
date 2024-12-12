fb81fb62a8fb4d196b8dbd48bde11f8206005a70
cristian <alexander85vega@gmail.com>
2024-12-12 20:32:58 -0300

    Agrega nuevos estilos

--- client/script.js
+++ client/script.js
@@ -4,11 +4,27 @@ const socket = io();
 
 const from = document.getElementById("form");
 const input = document.getElementById("input");
+const messages = document.getElementById("messages");
+
+socket.on("chat message", (msg) => {
+  const item = document.createElement("li");
+  item.textContent = msg;
+  messages.appendChild(item);
+});
 
 from.addEventListener("submit", (e) => {
   e.preventDefault();
   if (input.value) {
     socket.emit("chat message", input.value);
     input.value = "";
+    input.style.height = "1.5rem";
   }
+});
+
+input.addEventListener("input", (event) => {
+  const element = event.target;
+  element.style.height = "auto";
+  const newHeight = Math.min(element.scrollHeight, 225);
+  console.log(newHeight);
+  element.style.height = `${newHeight}px`;
 });
