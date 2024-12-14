a09ae0a7acc86260fcf8519d93809921aa858ca9
cristian <alexander85vega@gmail.com>
2024-12-14 20:53:02 -0300

    mejoras en el front-end

--- client/script.js
+++ client/script.js
@@ -3,10 +3,12 @@ import { EmojiButton } from "https://cdn.jsdelivr.net/npm/@joeattardi/emoji-butt
 
 const socket = io();
 
+const chat = document.querySelector(".chat");
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const messages = document.getElementById("messages");
 const emojiButton = document.getElementById("emoji-button");
+const windowButton = document.getElementById("fullscreen-button");
 
 // Configurar Emoji Button
 const picker = new EmojiButton({
@@ -21,7 +23,7 @@ emojiButton.addEventListener("click", () => {
 });
 
 picker.on("emoji", (selection) => {
-  input.value += selection.emoji; // Accede al emoji como texto
+  input.value += selection.emoji;
 });
 
 // Ajustar altura del textarea
@@ -32,19 +34,56 @@ input.addEventListener("input", (event) => {
   element.style.height = `${newHeight}px`;
 });
 
+input.addEventListener("focus", () => {
+  chat.classList.add("chat-adjusted");
+});
+
+input.addEventListener("blur", () => {
+  setTimeout(() => {
+    chat.classList.remove("chat-adjusted");
+  }, 100);
+});
+
+
 // Recibir mensajes desde el servidor
 socket.on("chat message", (msg) => {
   const item = document.createElement("li");
   item.textContent = msg;
   messages.appendChild(item);
   messages.scrollTop = messages.scrollHeight;
+  input.placeholder = "Escribe aquí...";
+  input.classList.remove("input-error")
+});
+
+socket.on("connect_error", (err) => {
+  console.error("Error de conexión con el servidor:", err.message);
+  input.classList.add("input-error");
+  input.placeholder = `Error de conexión con el servidor...`;
+});
+
+socket.on("disconnect", () => {
+  input.classList.add("input-error");
+  input.placeholder = `Desconectado del servidor. Intentando reconectar...`;
 });
 
 // Enviar mensaje
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (input.value.trim()) {
-    socket.emit("chat message", input.value); // Enviar al servidor
+    socket.emit("chat message", input.value);
     input.value = "";
+    input.style.height = `77px`;
+  }
+});
+
+windowButton.addEventListener("click", () => {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen().catch((err) => {
+      console.log(
+        `Error al intentar entrar en pantalla completa: ${err.message}`
+      );
+    });
+  } else {
+    document.exitFullscreen();
   }
 });
